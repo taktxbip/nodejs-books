9cad5d5e18ba85818ebbcd89fc43b3bf0d422af2
Evnomad <b.boy.bip@gmail.com>
2022-09-18 12:29:41 +0400

    make it better

--- utils/hbs-utils.js
+++ utils/hbs-utils.js
@@ -1,5 +1,11 @@
 module.exports = {
   ifeq(a, b, options) {
+    if (
+      typeof a === 'undefined' ||
+      typeof b === 'undefined'
+    ) {
+      return options.inverse(this);
+    }
     if (a.toString() === b.toString()) {
       return options.fn(this);
     }
